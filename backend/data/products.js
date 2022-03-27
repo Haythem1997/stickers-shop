0dc3177c489144ff8277712d1494afdadbe98ff4
haythem achour <achourhaythem6@gmail.com>
2022-03-27 16:11:28 +0200

    First commit

---
+++ backend/data/products.js
@@ -0,0 +1,179 @@
+const products = [
+
+  {
+    name: 'albert',
+    image: '/images/albert.png',
+    description:
+      'for people addicted to sience',
+    brand: 'ACH-product',
+    category: 'sience',
+    price: 0.500,
+    countInStock: 10,
+    rating: 4,
+    numReviews: 0,
+  },
+{
+    name: 'camping-fire',
+    image: '/images/camping-fire.png',
+    description:
+      'for people addicted to camp ',
+    brand: 'ACH-product',
+    category: 'camping',
+    price: 0.500,
+    countInStock: 10,
+    rating: 3,
+    numReviews: 0,
+  },
+{
+    name: 'goodbye-billie',
+    image: '/images/goodbye-billie.png',
+    description:
+      'for people addicted billie eilish',
+    brand: 'ACH-product',
+    category: 'music',
+    price: 0.500,
+    countInStock: 10,
+    rating: 4,
+    numReviews: 0,
+  },
+{
+    name: 'Meow',
+    image: '/images/Meow.png',
+    description:
+      'for people addicted to cats',
+    brand: 'ACH-product',
+    category: 'animals',
+    price: 0.500,
+    countInStock: 10,
+    rating: 4,
+    numReviews: 0,
+  },
+
+{
+    name: 'Naruto',
+    image: '/images/Naruto.png',
+    description:
+      'for people addicted to Naruto',
+    brand: 'ACH-product',
+    category: 'anime',
+    price: 0.500,
+    countInStock: 10,
+    rating: 4,
+    numReviews: 0,
+  },
+
+{
+    name: 'simpson',
+    image: '/images/simpson.png',
+    description:
+      'for people addicted to the simpson',
+    brand: 'ACH-product',
+    category: 'anime',
+    price: 0.500,
+    countInStock: 10,
+    rating: 4,
+    numReviews: 0,
+  },
+
+{
+    name: 'spongebob',
+    image: '/images/spongebob.png',
+    description:
+      'for people addicted to spongebob',
+    brand: 'ACH-product',
+    category: 'anime',
+    price: 0.500,
+    countInStock: 10,
+    rating: 4,
+    numReviews: 0,
+  },
+
+{
+    name: 'Weed-green-lips',
+    image: '/images/Weed-green-lips.png',
+    description:
+      'for people addicted to dirty things',
+    brand: 'ACH-product',
+    category: 'dirty',
+    price: 0.500,
+    countInStock: 10,
+    rating: 4,
+    numReviews: 0,
+  },
+{
+    name: '2-pack',
+    image: '/images/2-pack.png',
+    description:
+      'for people addicted to rap',
+    brand: 'ACH-product',
+    category: 'music',
+    price: 0.500,
+    countInStock: 10,
+    rating: 4,
+    numReviews: 0,
+  },
+{
+    name: 'Gaming',
+    image: '/images/Gaming.png',
+    description:
+      'for people addicted to Gaming',
+    brand: 'ACH-product',
+    category: 'Gaming',
+    price: 0.500,
+    countInStock: 10,
+    rating: 4,
+    numReviews: 0,
+  },
+{
+    name: 'Tokio-denver',
+    image: '/images/Tokio-denver.png',
+    description:
+      'for people addicted to Casadepapel',
+    brand: 'ACH-product',
+    category: 'Netflix',
+    price: 0.500,
+    countInStock: 10,
+    rating: 4,
+    numReviews: 0,
+  },
+{
+    name: 'jujutsu-kaisen-pack',
+    image: '/images/jujutsu-kaisen-pack.png',
+    description:
+      'Jujutsu Kaisen (呪術廻戦じゅじゅつかいせん?, litt. "Combat de sorcellerie") ',
+    brand: 'ACH-product',
+    category: 'anime',
+    price: 0.500,
+    countInStock: 10,
+    rating: 5,
+    numReviews: 0,
+  },
+{
+    name: 'my-hero-academia-pack',
+    image: '/images/my-hero-academia-pack.png',
+    description:
+      'My Hero Academia (僕のヒーローアカデミア, Boku no Hīrō Akademia?)',
+    brand: 'ACH-product',
+    category: 'anime',
+    price: 0.500,
+    countInStock: 10,
+    rating: 5,
+    numReviews: 0,
+  },
+
+{
+    name: 'detective-conan-pack',
+    image: '/images/detective-conan-pack.png',
+    description:
+      'detective-conan est une série japonaise de mangas créée par Gōshō Aoyama1. La série débute le 5 janvier 1994 ',
+    brand: 'ACH-product',
+    category: 'anime',
+    price: 0.500,
+    countInStock: 10,
+    rating: 5,
+    numReviews: 0,
+  },
+
+]
+
+export default products
